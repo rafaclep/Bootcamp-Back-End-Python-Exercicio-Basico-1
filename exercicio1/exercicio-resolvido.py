1a8b9d48ba589a8bc32e1659c208a44c056c5b3f
Alessandra Ribeiro Guimaraes <alessandra1ribeiroguimaraes@gmail.com>
2024-08-03 14:52:55 -0300

    Correção de código para atender o exercício

--- exercicio1/exercicio-resolvido.py
+++ exercicio1/exercicio-resolvido.py
@@ -4,23 +4,15 @@ operações soma, subtração, multiplicação, divisão.
 '''
 
 import os
-import re
 import platform
 from typing import Final
 from typing import Any
 
-LINHA_TRACEJADA: Final[str] = '-' * 31
 COR_BRANCA: Final[str] = '\033[0;0m'
 COR_BRIGHT_AMARELA: Final[str] = '\033[93m'
 COR_VERDE: Final[str] = '\033[32m'
 COR_BRIGHT_VERMELHA: Final[str] = '\033[91m'
-OPERADORES_E_TERMOS:  Final[dict[str, str ]] = {
-    '+': 'Soma',
-    '-': 'Subtração',
-    '*': 'Multiplicação',
-    '/': 'Divisão'
-}
-VALIDACAO_DA_FORMULA: Final[str] = r'^\s*\d{1,8}(?:\.\d{1,2}|)\s*[*+-/]\s*\d{1,8}(?:\.\d{1,2}|)\s*$'
+
 
 # Habilita os caracteres ANSI escape no terminal Windows.
 os.system("")
@@ -55,28 +47,6 @@ def limpar_console():
     if platform.system() == 'Linux':
         os.system('clear')
 
-def input_opcoes(msg: str, opcoes: dict[str, str ]) -> str:
-    '''
-    Obtem a opção válida.
-    Retorna a opção.
-    '''
-    while True:
-        opcao = input(msg).upper()
-        if opcao in opcoes:
-            return opcao
-        print(f"'{bright_vermelho(opcao)}'\tOpção inválida! As opções válidas são: {verde(', '.join(opcoes))}") # pylint: disable=line-too-long
-
-def input_int(msg: str) -> int:
-    '''
-    Obtem número inteiro informado pelo usuário.
-    Retorna o número.
-    '''
-    while True:
-        try:
-            return int(input(msg))
-        except ValueError:
-            print(bright_vermelho('\tApenas números inteiros são aceitos. Por favor, tente novamente.\n'))
-
 def input_float(msg: str) -> float:
     '''
     Obtem número informado pelo usuário.
@@ -88,64 +58,78 @@ def input_float(msg: str) -> float:
         except ValueError:
             print(bright_vermelho('\tApenas números são aceitos. Por favor, tente novamente.\n'))
 
-def exibir_menu(opcoes: dict[str, str]) -> None:
+def exibir_cabecalho() -> None:
     '''
-    Exibi o menu de opções.
+    Exibe o cabeçalho.
     '''
-    print('')
-    print(verde(f'\t{LINHA_TRACEJADA}'))
-    cabecalho = 'MENU DE OPÇÕES PARA O CÁLCULO\n'
-    print(verde(cabecalho.center(50)))
+    limpar_console()
+    print(bright_amarelo('\n\t*****************************************************************'))
+    print(bright_amarelo('\n\t** Calcular a soma, a subtração, a multiplicação e a divisão. **'))
+    print(bright_amarelo('\n\t*****************************************************************\n'))
 
-
-    for key, value in opcoes.items():
-        opcao = '|' + key + '|' + "  "  + value
-        print(f"\t\t{verde(opcao)} ")
-    print(verde(f'\t{LINHA_TRACEJADA}'))
-
-def calcular_operacoes(calculo:str) -> float:
+def obter_numero(numero: int) -> float:
     '''
-    Calcula a operação aritmética de acordo com o operador e os numeros informadas pelo usuário.
-    Retorna o resultado da operação.
-    '''
-    if '*' in calculo:
-        numero_1, numero_2 = calculo.split("*")
-        return float(numero_1) * float(numero_2)
-    elif '/' in calculo:
-        numero_1, numero_2 = calculo.split("/")
-        return float(numero_1) / float(numero_2)
-    elif '-' in calculo:
-        numero_1, numero_2 = calculo.split("-")
-        return float(numero_1) - float(numero_2)
-    elif '+' in calculo:
-        numero_1, numero_2 = calculo.split("+")
-        return float(numero_1) + float(numero_2)
-    raise ValueError(bright_vermelho(f'\tNão é possível realizar cálculo para o operador = {calculo}'))
-
-
-def input_calculo() -> str:
-    '''
-    Obtem as entrada para o calculo.
-    Retorna o calculo.
+    Obtem um número válido.
+    Retorna o número.
     '''
     while True:
-        calculo = input("\tCalcular: ")
-        calculo_valido = re.match(VALIDACAO_DA_FORMULA, calculo)
-        if calculo_valido:
-            return calculo
-        print(bright_vermelho(f'\tNão é possível realizar cálculo {calculo}. Por favor, tente novamente.'))
+        numero_informado = input_float(f"\n\t{numero}° número: ")
+        return numero_informado
 
+def exibir_calculos(numeros: list[float]) -> None:
+    '''
+   Exibe os cáculos de soma, subtração, multiplicação e divisão no console.
+    '''
+    exibir_cabecalho()
+    soma = numeros[0] + numeros[1]
+    print(bright_amarelo("\tSoma"))
+    print(bright_amarelo("\t****************"))
+    print(f"\t{numeros[0]} + {numeros[1]} = {soma}")
+    print(bright_amarelo("\t****************\n"))
+
+    subtracao = numeros[0] - numeros[1]
+    print(bright_amarelo("\tSubtração"))
+    print(bright_amarelo("\t****************"))
+    print(f"\t{numeros[0]} - {numeros[1]} = {subtracao}")
+    print(bright_amarelo("\t****************\n"))
+
+    multiplicacao = numeros[0] * numeros[1]
+    print(bright_amarelo("\tMultiplicação"))
+    print(bright_amarelo("\t****************"))
+    print(f"\t{numeros[0]} x {numeros[1]} = {multiplicacao}")
+    print(bright_amarelo("\t****************\n"))
+ 
+    if numeros[1] == 0:
+        print(bright_amarelo("\tDivisão"))
+        print(bright_amarelo("\t*******************************************************"))
+        print(bright_vermelho(f"\t{numeros[0]} / {numeros[1]} = Não é possível dividir por zero."))
+        print(bright_amarelo("\t********************************************************\n"))
+    else:
+        divisao = numeros[0] / numeros[1]
+        print(bright_amarelo("\tDivisão"))
+        print(bright_amarelo("\t****************"))
+        print(f"\t{numeros[0]} / {numeros[1]} = {divisao:.2f}")
+        print(bright_amarelo("\t****************\n"))
+
+def obter_numeros(qtd_numeros_informados: int = 2) -> list[float]:
+    '''
+     Obtem a quantidade de números inseridos pelo usuário na ordem ['1°', '2°'].
+     Retorna uma lista com os números.
+    '''
+    numeros: list[float] = []
+    for numero in range(1, qtd_numeros_informados+1):
+        numero = obter_numero(numero)
+        numeros.append(numero)
+    return numeros
 
 def main() -> None:
     '''
     Fluxo principal do programa.
     '''
-    limpar_console()
-    print(verde('\t\tFaça seu cálculo.'))
-    exibir_menu(OPERADORES_E_TERMOS)
-    calculo = input_calculo()
-    limpar_console()
-    print(bright_amarelo(f"\n\t O resultado de {calculo} é: {calcular_operacoes(calculo):.2f}"))
+    exibir_cabecalho()
+    numero = obter_numeros()
+    exibir_calculos(numero)
+
 
 if __name__ == '__main__':
     main()
